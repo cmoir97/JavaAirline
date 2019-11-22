ebff25e8249d84d3c0db3c0a6488afca5d75b2b6
Cameron Moir <cameronjmoir@gmail.com>
2019-11-22 10:34:55 +0000

    tested for passenger name

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    Passenger passenger1;
+
+    @Before
+    public void before(){
+        passenger1 = new Passenger("Cameron", 2);
+    }
+
+    @Test
+    public void canGetName(){
+        assertEquals("Cameron", passenger1.getName());
+    }
+}
